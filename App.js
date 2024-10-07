fea9b98d9b7f236493a4fbe0de38f1b284d58751
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2024-10-07 16:53:59 +0300

    Add font and Txt compoennt

--- App.js
+++ App.js
@@ -3,7 +3,50 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Home } from "./pages/Home/Home";
 import { ImageBackground } from "react-native";
 import backgroundImg from "./assets/background.png";
+import { useEffect, useState } from "react";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
+import { MeteoAPI } from "./api/meteo";
+import { useFonts } from "expo-font";
+
 export default function App() {
+  const [coordinates, setCoordinates] = useState();
+  const [weather, setWeather] = useState();
+  const [isFontLoaded] = useFonts({
+    "Alata-Regular": require("./assets/font/Alata-Regular.ttf"),
+  });
+
+  console.log(isFontLoaded);
+  useEffect(() => {
+    getUserCoordinates();
+  }, []);
+
+  useEffect(() => {
+    if (coordinates) {
+      fetchWeatherByCoords(coordinates);
+    }
+  }, [coordinates]);
+
+  async function fetchWeatherByCoords(coords) {
+    const weatherResponse = await MeteoAPI.fetchWeatherByCoords(coords);
+    setWeather(weatherResponse);
+  }
+
+  async function getUserCoordinates() {
+    const { status } = await requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoordinates({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    } else {
+      setCoordinates({ lat: "48.85", lng: "2.35" });
+    }
+  }
+
   return (
     <ImageBackground
       imageStyle={s.img}
@@ -12,7 +55,7 @@ export default function App() {
     >
       <SafeAreaProvider>
         <SafeAreaView style={s.container}>
-          <Home />
+          {isFontLoaded && <Home />}
         </SafeAreaView>
       </SafeAreaProvider>
     </ImageBackground>
--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,16 +1,17 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { s } from "./Home.style";
+import { Txt } from "../../components/Txt/Txt";
 export function Home() {
   return (
     <>
       <View style={s.meteo_basic}>
-        <Text style={s.txt}>Basic Weather info</Text>
+        <Txt style={{ fontSize: 70 }}>Hello</Txt>
       </View>
       <View style={s.searchbar_container}>
-        <Text style={s.txt}>SearchBar</Text>
+        <Txt>SearchBar</Txt>
       </View>
       <View style={s.meteo_advanced}>
-        <Text style={s.txt}>Advanded Weather info</Text>
+        <Txt>Advanded Weather info</Txt>
       </View>
     </>
   );
